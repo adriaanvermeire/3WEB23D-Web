7657750c579d4da8740c72b4ceb3dcc6bd429079
adriaanvermeire <adriaan.vermeire@student.kdg.be>
2018-04-19 11:38:35 +0200

    Add and use master layout, also add component template for websection & contentblock

---
+++ seksueleIntimidatie/resources/views/components/contentblock.blade.php
@@ -0,0 +1,4 @@
+{{-- Gebruik slot voor in deze component: https://laravel.com/docs/5.5/blade#components-and-slots --}}
+<div class="contentblock">
+
+</div>
--- seksueleIntimidatie/resources/views/index.blade.php
+++ seksueleIntimidatie/resources/views/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Seksuele Intimidatie Scholieren</title>
-</head>
-<body>
-  <h1>Seksuele Intimidatie</h1>
-</body>
-</html>
+@extends('layouts/master')
+
+@section('title', 'Seksuele Intimidatie')
+
+@section('content')
+ <h1>Index</h1>
+@endsection
