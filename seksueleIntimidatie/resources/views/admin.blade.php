0dfa2693c8ee97fa5848a40d485b3323dbef3490
MaximGns <maxim.ganses@student.kdg.be>
2018-04-26 09:43:56 +0200

    admin panel voor presentatie

--- seksueleIntimidatie/resources/views/admin.blade.php
+++ seksueleIntimidatie/resources/views/admin.blade.php
@@ -4,30 +4,35 @@
 
 @section('content')
 
-
+<h1 class="text-center mt-5">Admin-panel</h1>
 
 @foreach ($websection as $section)
    
-<div class="container-fluid">
+<div class="container-fluid mt-5">
+  
     <h3>Sectieomschrijving: {{ $section->name }}</h3>
     <div class="col-sm">
-        <ul>
+       <!-- <ul>
             <li>
             <p>gemaakt op: {{$section->created_at}}</p>
-            <p>upgedate op: {{$section->updated_at}}</p>
+            <p>upgedate op: {{$section->updated_at}}</p> 
             </li>
-        </ul>
+        </ul>-->
     </div>
 
     <div class="col-sm">
-        <a href="/edit/{{$section->id}}">
-        <button class="btn btn-warning">Edit</button>
-    </a>
+       
     {!! Form::open(['action' => 'AdminController@delete_data']) !!}
 
 
     {{ Form::hidden('id', $section->id) }}   
-    {!! Form::submit('Delete this post!') !!}
+    <div class="btn-group">
+        <a href="/edit/{{$section->id}}" class='btn btn-warning'>
+        Edit
+        </a>
+        <button class="btn btn-danger" type='submit'>Delete</button>
+    </div>
+
     {!! Form::close() !!}
 
     </div>
@@ -35,16 +40,19 @@
 
 </div>
 @endforeach
-<button type="submit" class="btn btn-primary">yowtow</button>
 
-<h1>Nieuwe sectie aanmaken</h1>
+<br>
+<br>
+
+
+<h1 class="text-center">Nieuwe sectie aanmaken</h1>
 {!! Form::open(['action' => 'AdminController@save_data']) !!}
 
 
-{!! Form::label('name', 'Websectie omschrijving', ['class' => 'col-md-4 control-label', 'id' => 'name_label']) !!}
+{!! Form::label('name', 'Websectie omschrijving', ['class' => 'col-md-4 control-label text-right', 'id' => 'name_label']) !!}
 {!! Form::text('name', '', ['class' => 'col-md-4 control-label', 'id' => 'name_label']) !!}
 
-{!! Form::submit('Click Me!') !!}
+{!! Form::submit('Toevoegen',['class' => 'btn btn-success']) !!}
 {!! Form::close() !!}
 
 
--- seksueleIntimidatie/resources/views/edit.blade.php
+++ seksueleIntimidatie/resources/views/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts/master') @section('title', 'edit post') @section('content')
-<h1>Edit post</h1>
+
 
 
 <div class="container text-center">
     <div class="text-center">
-        <h1>Wijzigen informatie</h1>
+        <h1 class="m-5">Wijzigen van websectie</h1>
     </div>
 
-    <div class="card card-default text-center">
+    <div class="card card-default text-center py-5">
 
 
 
@@ -18,15 +18,15 @@
         {!! Form::open(['action' => 'AdminController@edit_data']) !!}
 
         <!-- name -->
-        {{ Form::label('name', 'Name') }}
-        {{ Form::text('name') }}
+        {{ Form::label('name', 'Omschrijving') }}
+        {{ Form::text('name',$websection->name, ['class' => 'col-md-4 control-label', 'id' => 'name_label']) }}
 
         {{ Form::hidden('id', $websection->id) }}   
 
         <!-- email -->
             
 
-        {{ Form::submit('Update websection!') }}
+        {{ Form::submit('Wijzigen', ['class' => 'btn btn-success']) }}
 
     {{ Form::close() }}
     </div>
