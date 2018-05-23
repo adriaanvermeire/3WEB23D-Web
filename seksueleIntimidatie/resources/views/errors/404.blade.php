3db897ef944986b177a33d06390db107539f750a
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-23 21:09:15 +0200

    add basic layout for 404 page, maybe more to come!

--- seksueleIntimidatie/resources/views/errors/404.blade.php
+++ seksueleIntimidatie/resources/views/errors/404.blade.php
@@ -3,4 +3,14 @@
 @section('title', 'Deze pagina vinden we even niet...')
 
 @section('content')
+<div class="container mt-5">
+  <div class="row justify-content-center d-flex">
+    <div class="col text-center">
+      <h1>Oei! Deze pagina vinden we even niet...</h1>
+      <a href="{!! route('home') !!}">
+        <img src="{!! asset('images/ga-terug-naar-start.jpg') !!}" alt="Ga terug naar start" class='img-fluid image-404'>
+      </a>
+    </div>
+  </div>
+</div>
 @endsection
