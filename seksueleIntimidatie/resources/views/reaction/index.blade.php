07cca6cc8374c4d92ae5b1ea9ec562813c62031f
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-24 10:11:44 +0200

    begin layout

--- seksueleIntimidatie/resources/views/reaction/index.blade.php
+++ seksueleIntimidatie/resources/views/reaction/index.blade.php
@@ -11,38 +11,36 @@
   </div>
 
   <div class="container wat">
-    <div class="row">
-      <button data-toggle="collapse" data-target="#demo" class="btn-block buttonNormal shadow text-left vraag p-2 pr-5 pointer">
-        <h5>
-          <strong>
-            Ik was echt bang voor een jongen uit het rugbyteam. Hij moest en zou me ’hebben’. Hij viel me een jaar lang zowat elke dag
-            lastig. Toen we op een dag alleen in het klaslokaal waren, kwam hij op me af. Ik sprong overeind en rende de
-            klas uit.
-          </strong>
-        </h5>
-        <i class="fa fa-angle-down pull float-right"></i>
-      </button>
-
-      <div id="demo" class="collapse">
-        <div class="container wide shadow bgwhite p-3 mt-3">
-          <div class="row p-2">
-            <div class="col-2 offset-sm-1">
-              <i class="fas fa-plus-circle"></i>
-            </div>
-            <div class="col-6">
-              Weersta de verleiding om er maar een beetje om te lachen. Laat duidelijk merken, ook door je gezichtsuitdrukking,
-              wat je wel en niet zult tolereren.</div>
+    <div class="card shadow">
+        <a data-toggle="collapse" href="#test-block" aria-expanded="true" aria-controls="test-block">
+          <div class="card-header d-flex justify-content-between">
+            <h5 class='text-dark'>
+              <strong>
+                Ik was echt bang voor een jongen uit het rugbyteam. Hij moest en zou me ’hebben’. Hij viel me een jaar lang zowat elke dag
+                lastig.
+              </strong>
+            </h5>
+            <i class="fa fa-angle-down pull float-right"></i>
           </div>
-          <div class="row p-2">
-            <div class="col-2 offset-sm-1">
-              <i class="fas fa-minus-circle"></i>
+        </a>
+        <div id="test-block" class="collapse">
+            <div class="card-block">
+              <div class="row p-2">
+                <div class="col-2 offset-sm-1">
+                  <i class="fas fa-plus-circle"></i>
+                </div>
+                <div class="col-6">
+                  Weersta de verleiding om er maar een beetje om te lachen. Laat duidelijk merken, ook door je gezichtsuitdrukking,
+                  wat je wel en niet zult tolereren.</div>
+              </div>
+              <div class="row p-2">
+                <div class="col-2 offset-sm-1">
+                  <i class="fas fa-minus-circle"></i>
+                </div>
+                <div class="col-6">De kans is klein dat iemand zijn gedrag verandert als iedereen het acceptabel vindt.</div>
+              </div>
             </div>
-            <div class="col-6">De kans is klein dat iemand zijn gedrag verandert als iedereen het acceptabel vindt.</div>
-          </div>
-
-
         </div>
-      </div>
     </div>
   </div>
 
@@ -56,6 +54,8 @@
 
     </div>
   </div>
+  @push('scripts')
+
   <script>
     $(function () { //run when the DOM is ready
       $(".vraag").click(function () { //use a class, since your ID gets mangled
@@ -73,6 +73,7 @@
       });
     });
   </script>
+@endpush
 
 
 @endsection
