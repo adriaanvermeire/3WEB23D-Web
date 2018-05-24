4d4353d628f640673d3f7de9b35d6c37ea7b7b1e
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-24 09:13:52 +0200

    add layout for success page

--- seksueleIntimidatie/resources/views/testimonials/success.blade.php
+++ seksueleIntimidatie/resources/views/testimonials/success.blade.php
@@ -3,4 +3,18 @@
 @section('title', $title)
 
 @section('content')
+  <div class="container mt-5">
+    <h1 class="text-center red">Gelukt!</h1>
+  </div>
+
+  <div class="text-center">
+      <i class="fas fa-thumbs-up green fa-10x"></i>
+  </div>
+  <div class="text-center" style="margin-top: 10vh;">
+      <h2>Bedankt voor jouw verhaal.</h2>
+      <h2>We behandelen het in alle vertrouwen.</h2>
+  </div>
+  <div class="text-center" style="margin-top: 10vh; margin-bottom: 10vh;">
+    <a href="{!! route('testimonials.index') !!}" class="btn buttonBlue shadow">Bekijk alle verhalen</a>
+  </div>
 @endsection
