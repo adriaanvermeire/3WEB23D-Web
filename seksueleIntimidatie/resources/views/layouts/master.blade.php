f077865eb098a166fc4e1d97db2dd9ac21324749
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-24 09:50:19 +0200

    add bootstrap js

--- seksueleIntimidatie/resources/views/layouts/master.blade.php
+++ seksueleIntimidatie/resources/views/layouts/master.blade.php
@@ -9,8 +9,7 @@
   <link href="{{ asset('css/homenav.css') }}" rel="stylesheet" type="text/css" >
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg"
       crossorigin="anonymous">
-
-  <title>@yield('title')</title>
+        <title>@yield('title')</title>
 </head>
 @php
   if(isset($color)){
@@ -33,6 +32,8 @@
   @endif
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
     crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" charset="utf-8"></script>
+
   @stack('scripts')
 </body>
 </html>
