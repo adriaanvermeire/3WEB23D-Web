744b22a9c00614eab42a5f535c97ddcfb4e39cb8
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-24 15:05:24 +0200

    show reactions on admin panel

--- seksueleIntimidatie/resources/views/admin/index.blade.php
+++ seksueleIntimidatie/resources/views/admin/index.blade.php
@@ -52,9 +52,40 @@
   <div class="card shadow mt-2">
     <div class="card-body">
       <div class="card-title">
-        <h2>Hoe reageren?</h2>
+        <span class=" d-flex justify-content-between align-items-center">
+          <h2>Hoe reageren?</h2>
+          <a class="btn btn-success ml-2 text-white" href='{!! route('admin.create-scenario') !!}'><i class="fas fa-plus"></i> Toevoegen</a>
+        </span>
         <p>Hier vind je een overzicht van alle situaties die de gebruikers te zien krijgen op de 'Hoe reageren?'-pagina. Elk scenario heeft een titel (de omschrijving), een goede reactie en een slechte reactie.</p>
-        </div>
+      </div>
+      <div class="card-text">
+        @if(count($scenarios) > 0)
+        <table class="table">
+          <th></th>
+          <th>Titel</th>
+          <th>Goede reactie</th>
+          <th>Slechte reactie</th>
+          <th></th>
+          @foreach ($scenarios as $scenario)
+          <tr>
+            <td>
+              <a href='{!! route('admin.edit-scenario', ['id' => $scenario->id]) !!}' class="btn btn-warning btn-sm">Bewerken</a>
+            </td>
+            <td>@truncate($scenario->name, 50)</td>
+            <td>@truncate($scenario->good, 50)</td>
+            <td>@truncate($scenario->bad, 50)</td>
+            <td>
+              {{ Form::open(['route' => ['admin.delete-scenario', $scenario->id], 'method' => 'delete']) }}
+              <button type="submit" class='btn btn-danger'>Verwijderen</button>
+              {{ Form::close() }}
+            </td>
+          </tr>
+          @endforeach
+        </table>
+        @else
+        <p>Er zijn nog geen scenario's toegevoegd! Voeg er een paar om op de site te tonen.</p>
+        @endif
+      </div>
     </div>
   </div>
   <div class="card shadow mt-2">
@@ -64,14 +95,14 @@
         <p>Hier heb je een overzicht van alle verhalen. Indien er nieuwe verhalen zijn, dienen ze nagekeken te worden en vervolgens gepubliceerd of weerhouden worden.</p>
       </div>
       <div class="card-text">
+      <table class='table'>
+        <th></th>
+        <th></th>
+        <th>Titel</th>
+        <th>Content</th>
+        <th>Gepost op</th>
+        <th></th>
       @if (isset($unseen))
-        <table class='table'>
-          <th></th>
-          <th></th>
-          <th>Titel</th>
-          <th>Content</th>
-          <th>Gepost op</th>
-          <th></th>
         @foreach ($unseen as $testimonial)
           <tr>
             <td title='Dit verhaal moet nagekeken worden.'><i class="fas fa-exclamation-circle red"></i></td>
