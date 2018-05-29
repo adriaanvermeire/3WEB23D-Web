fdf5a479b83acf4e29717374d461cf56b0ca924d
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-29 21:39:42 +0200

    add mutator for section field of contactsection model

--- seksueleIntimidatie/app/ContactSection.php
+++ seksueleIntimidatie/app/ContactSection.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactSection extends Model
 {
+  // https://laravel.com/docs/5.6/eloquent-mutators#accessors-and-mutators
+  // Kind of a middleware for setting the section field
+  public function setSectionAttribute($value) {
+    if ( empty($value) ) {
+      $this->attributes['section'] = NULL;
+    } else {
+      $this->attributes['section'] = $value;
+    }
+  }
 }
