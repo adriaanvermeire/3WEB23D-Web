c6c6ca2cfc4f3075b75c12b55ea7bc710879084c
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-29 22:50:35 +0200

    implement panel components

--- seksueleIntimidatie/resources/views/admin/components/contact.blade.php
+++ seksueleIntimidatie/resources/views/admin/components/contact.blade.php
@@ -1,8 +1,32 @@
-<div id='contact' class="card shadow mt-2">
-  <div class="card-body">
-    <div class="card-title">
-      <h2>Waar terecht?</h2>
-      <p>Hier zie je alle secties van de 'Waar terecht?'-pagina. Elke sectie heeft een titel en content.</p>
-    </div>
-  </div>
+<div id='contact'>
+  @component('admin.components.adminSection')
+    @slot('sectionTitle')
+      Waar terecht?
+    @endslot
+    @slot('buttons')
+    @endslot
+    @slot('description')
+      Hier zie je alle secties van de 'Waar terecht?'-pagina. Elke sectie heeft een titel en content.
+    @endslot
+    @slot('body')
+      @if(count($contactSections) > 0)
+        <table class="table">
+          <th></th>
+          <th>Titel</th>
+          <th>Inhoud</th>
+          @foreach ($contactSections as $section)
+            <tr>
+              <td>
+                <a href='{!! route('admin.edit-content', ['id' => $section->id]) !!}' class="btn btn-warning btn-sm">Bewerken</a>
+              </td>
+              <td>@truncate($scenario->title, 50)</td>
+              <td>@truncate($scenario->body, 50)</td>
+            </tr>
+          @endforeach
+        </table>
+      @else
+        <p><strong>Er zijn geen secties voor deze pagina! Voeg er een paar toe om op de site te tonen.</strong></p>
+      @endif
+    @endslot
+  @endcomponent
 </div>
--- seksueleIntimidatie/resources/views/admin/components/what.blade.php
+++ seksueleIntimidatie/resources/views/admin/components/what.blade.php
@@ -1,8 +1,32 @@
-<div id='what' class="card shadow mt-2">
-  <div class="card-body">
-    <div class="card-title">
-      <h2>Wat is het?</h2>
-      <p>Hier zie je alle secties van de 'Wat is het?'-pagina. Elke sectie heeft een titel en content.</p>
-    </div>
-  </div>
+<div id='what'>
+  @component('admin.components.adminSection')
+    @slot('sectionTitle')
+      Wat is het?
+    @endslot
+    @slot('buttons')
+    @endslot
+    @slot('description')
+      Hier zie je alle secties van de 'Wat is het?'-pagina. Elke sectie heeft een titel en content.
+    @endslot
+    @slot('body')
+      @if (count($whatSections) > 0)
+        <table class="table">
+          <th></th>
+          <th>Titel</th>
+          <th>Inhoud</th>
+          @foreach ($whatSections as $section)
+            <tr>
+              <td>
+                <a href='{!! route('admin.edit-content', ['id' => $section->id]) !!}' class="btn btn-warning btn-sm">Bewerken</a>
+              </td>
+              <td>@truncate($scenario->title, 50)</td>
+              <td>@truncate($scenario->body, 50)</td>
+            </tr>
+          @endforeach
+        </table>
+      @else
+        <p><strong>Er zijn nog geen secties voor deze pagina! Voeg er een paar toe om op de site te tonen.</strong></p>
+      @endif
+    @endslot
+  @endcomponent
 </div>
