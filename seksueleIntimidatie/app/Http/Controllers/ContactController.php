5a5aca70cf72656251a90e042c593e87c623d7bb
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-30 11:12:50 +0200

    retrieve and send content for/to contactpage

--- seksueleIntimidatie/app/Http/Controllers/ContactController.php
+++ seksueleIntimidatie/app/Http/Controllers/ContactController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Content;
 
 class ContactController extends Controller
 {
     public $color = 'red';
 
     public function index(){
-      return view('contact.index', ['title' => 'Waar kan je terecht?', 'color' => $this->color]);
+      $content = Content::all();
+
+      return view('contact.index', ['content' => $content, 'title' => 'Waar kan je terecht?', 'color' => $this->color]);
     }
 }
