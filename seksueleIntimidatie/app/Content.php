54a3d2aff628ab1208689d5727b4b6d7cd022939
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-30 11:09:52 +0200

    add accessor to get the pretty page name for a contentsection

--- seksueleIntimidatie/app/Content.php
+++ seksueleIntimidatie/app/Content.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
 {
-    //
+  /**
+  * Get the webpage the content belongs to
+  *
+  * @param  string  $value
+  * @return string
+  */
+ public function getWebpageAttribute($value)
+ {
+     return $value === 'what'?'Wat is het?':'Waar terecht?';
+ }
+
 }
