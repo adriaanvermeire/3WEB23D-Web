bc3756252fd55a76d6bf42384628dbdcc183b9de
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-31 00:29:16 +0200

    refactor and clean code

--- seksueleIntimidatie/resources/views/what/index.blade.php
+++ seksueleIntimidatie/resources/views/what/index.blade.php
@@ -15,70 +15,37 @@
     <div class="col-md-4  ">
       {!! $content[0]->body !!}
     </div>
-
   </div>
 </div>
 <div class="container ">
-
   <div class="row">
     <div class="col-md-4 float-left shadow " style="height: 40vh; background-color:grey;">
       fotoke
     </div>
-
     <div class="col-md-6 offset-md-2 float-right">
       {!! $content[1]->body !!}
     </div>
   </div>
 </div>
 
-
 <div class="container">
   <div class="row">
     <h2>Welke vormen zijn er</h2>
   </div>
-
-  <div class="row d-flex justify-content-between">
-    <div class="col-md-3 ">
-      <div class="row">
-        <h3 class="red">    {{ $content[2]->title }}</h3>
+  <div class="card-deck">
+    @php
+      $range = range(2,4);
+    @endphp
+    @foreach ($range as $num)
+      <div class="card shadow">
+        <div class="card-body">
+          <div class="card-title red">
+            <h3>{{ $content[$num]->title }}</h3>
+          </div>
+          <div class="card-text">{!! $content[$num]->body !!}</div>
+        </div>
       </div>
-      <div class="row shadow bg-white ">
-
-
-        <ul class="list-group ">
-          
-          {!! $content[2]->body !!}
-        </ul>
-
-      </div>
-    </div>
-    <div class="col-md-3   ">
-      <div class="row">
-        <h3 class="red"> {{ $content[3]->title }} </h3>
-      </div>
-      <div class="row shadow bg-white">
-        <ul class="list-group ">
-          {!! $content[3]->body !!}
-
-
-        </ul>
-      </div>
-    </div>
-
-    <div class="col-md-3  ">
-      <div class="row">
-        <h2 class="red">{{ $content[4]->title }}</h2>
-
-      </div>
-      <div class="row shadow bg-white">
-        <ul class="list-group  ">
-      
-          {!! $content[4]->body !!}
-        </ul>
-      </div>
-    </div>
-
-
+    @endforeach
   </div>
 </div>
 <div class="container">
@@ -87,14 +54,12 @@
       <h2 class="float-right">{{ $content[5]->title }}</h2>
     </div>
   </div>
-
   {!! $content[5]->body !!}
 </div>
 <div class="container">
   <div class="row">
     <div class="container">
       <h2 class="">{{ $content[6]->title }} </h2>
-      
       {!! $content[6]->body !!}
     </div>
   </div>
@@ -116,18 +81,15 @@
 </div>
 
 <div class="container">
-
  <h2> {{ $content[8]->title }} </h2>
-  <div class="row  d-flex justify-content-between">
-    <div class="col-md-4 ml-0 "> {!! $content[8]->body !!} 
+  <div class="row d-flex justify-content-between">
+    <div class="col-md-4 ml-0 "> {!! $content[8]->body !!}
     </div>
-
-    <div class="col-md-4   shadow " style="height: 40vh; background-color:grey;">
+    <div class="col-md-4 shadow " style="height: 40vh; background-color:grey;">
       fotoke
     </div>
   </div>
 </div>
-
 <div class="container">
   <div class="row">
     <div class="container">
@@ -138,66 +100,46 @@
     <div class="col-md-4 text-center mb-5">
       <h4 class="text-center red">{{ $content[10]->title }}</h4>
       <p class="text-center pt-5 ">{!! $content[10]->body !!}  </p>
-
       <img src="{!! asset('images/statistiek12.png') !!}" class="img-fluid mb-3 " alt="">
     </div>
-
-
     <div class="col-md-4 text-center mb-5 ">
-
       <h4 class="text-center red ">{{ $content[11]->title }}</h4>
       <p class="text-center pt-5 mb-5 ">{!! $content[11]->body !!}  </p>
-      
       <h5>3 op de 10 meisjes</h5>
-      @for ($i = 0; $i
-      < 10; $i++) @if ($i < 3) <i class="fas fa-female red">
-        </i>
+      @for ($i = 0; $i < 10; $i++)
+        @if ($i < 3)
+          <i class="fas fa-female red"></i>
         @else
-        <i class="fas fa-female"></i>
-        @endif @endfor
+          <i class="fas fa-female"></i>
+        @endif
+      @endfor
         <br>
         <br>
         <h5>3 op de 100 jongens</h5>
-        @for ($i = 0; $i
-        < 5; $i++) @for ($y=0 ; $y < 20; $y++) @if ($i==0 && $y < 3 ) <i class="fas fa-male red">
-          </i>
+      @for ($i = 0; $i < 5; $i++)
+        @for ($y=0 ; $y < 20; $y++)
+          @if ($i==0 && $y < 3 )
+            <i class="fas fa-male red"></i>
           @else
-          <i class="fas fa-male"></i>
-          @endif @endfor
-          <br> @endfor
+            <i class="fas fa-male"></i>
+          @endif
+        @endfor
+          <br>
+      @endfor
     </div>
-
-
     <div class="col-md-4 mb-5 ">
-
       <h4 class="text-center red "> {{ $content[12]->title }}</h4>
-     
-     
       <p class="text-center pt-5">{!! $content[12]->body !!} </p>
- 
       <img src="{!! asset('images/statistiek3-2.png') !!}" class="img-fluid pl-5 pr-5 mb-3 " alt="">
-  
-     
     </div>
   </div>
-
 </div>
-
-
-
-
-
-
-
 <div class="container">
   <div class="row">
     <div class="container">
-
       <h2 class=""> {{ $content[13]->title }} </h2>
-
-      {!! $content[13]->body !!} 
-
-      <a href="/waar-kan-je-terecht" class="btn buttonBlue shadow link white mt-5"> Klik voor meer info </a>
+      {!! $content[13]->body !!}
+      <a href="{!! route('contact.index') !!}" class="btn buttonBlue shadow link white mt-5"> Klik voor meer info </a>
     </div>
   </div>
 
