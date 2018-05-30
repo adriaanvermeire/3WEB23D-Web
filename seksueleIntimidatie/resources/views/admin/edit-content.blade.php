0d25ba02ae8b799804cbd29a90f69ba04e023de4
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-30 11:11:27 +0200

    add view for editing a content section

--- seksueleIntimidatie/resources/views/admin/edit-content.blade.php
+++ seksueleIntimidatie/resources/views/admin/edit-content.blade.php
@@ -3,7 +3,28 @@
 @section('title', 'Admin | Content bewerken')
 
 @section('content')
-<div class="container">
-
-</div>
+  <div class="container mt-5">
+  <h1 class="text-center red">Admin | Content bewerken</h1>
+  </div>
+  <div class="container mt-5 mb-3">
+    <a href="{!! route('admin.index') !!}" class="btn buttonBlue shadow link white"><i class="fas fa-arrow-left"></i> Admin Panel</a>
+  </div>
+  <div class="container mt-2">
+        <p><strong>Dit contentblok is van de '{{ $content->webpage }}'-pagina.</strong></p>
+        <form action="{!! route('admin.post-content') !!}" method="post">
+          @csrf
+          <div class="form-group">
+              <label for="title">Titel:</label>
+              <input name="title"  type="text" class="form-control shadow"
+              id="title" aria-describedby="title" placeholder="Omschrijving van het scenario"
+              value="{{ $content->title }}">
+          </div>
+          <div class="form-group">
+            <label for="body">Inhoud: </label>
+            <textarea id="body" name="body" class="form-control shadow" rows="10">{{ $content->body }}</textarea>
+          </div>
+          <input type="hidden" name="id" value="{{ $content->id }}">
+          <button type="submit" class="btn buttonGreen shadow">Sla content op</button>
+        </form>
+  </div>
 @endsection
