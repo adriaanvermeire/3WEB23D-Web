31e27e491a6aaaf0bcef04e04a4d7e0cab88382e
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-31 22:43:30 +0200

    clean up contactcontroller

--- seksueleIntimidatie/app/Http/Controllers/ContactController.php
+++ seksueleIntimidatie/app/Http/Controllers/ContactController.php
@@ -8,9 +8,17 @@ use App\Content;
 class ContactController extends Controller
 {
     public $color = 'red';
+    public $title = 'Waar kan je terecht?';
 
+    public function index()
+    {
       $content = Content::where('page', 'contact')->get();
 
-      return view('contact.index', ['content' => $content, 'title' => 'Waar kan je terecht?', 'color' => $this->color]);
+      $data = [
+        'content' => $content, 
+        'title' => $this->title, 
+        'color' => $this->color,
+      ];
+      return view('contact.index', $data);
     }
 }
