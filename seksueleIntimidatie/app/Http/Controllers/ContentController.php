65fea9e5441e03922b6f02d737552112cb0be87a
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-31 22:44:33 +0200

    clean up contentcontroller

--- seksueleIntimidatie/app/Http/Controllers/ContentController.php
+++ seksueleIntimidatie/app/Http/Controllers/ContentController.php
@@ -8,15 +8,19 @@ use Purify;
 
 class ContentController extends Controller
 {
-    public function edit($id){
+    public function edit($id)
+    {
       $poc = Content::findOrFail($id);
       return view('admin.edit-content', ['content' => $poc]);
     }
 
+    public function update(Request $request)
+    {
       $request->validate([
         'title' => 'required|string|min:4|unique:contents,'.$request->id,
         'body' => 'required|string|min:20',
       ]);
+      
       $content = Content::findOrFail($request->id);
       $content->title = $request->title;
       $content->body = Purify::clean($request->body);
