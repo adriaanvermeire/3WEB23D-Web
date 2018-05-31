91636e17a1faca955cf65e445825117aabe5c532
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-05-31 22:43:13 +0200

    fix query for content

--- seksueleIntimidatie/app/Http/Controllers/ContactController.php
+++ seksueleIntimidatie/app/Http/Controllers/ContactController.php
@@ -9,8 +9,7 @@ class ContactController extends Controller
 {
     public $color = 'red';
 
-    public function index(){
-      $content = Content::all();
+      $content = Content::where('page', 'contact')->get();
 
       return view('contact.index', ['content' => $content, 'title' => 'Waar kan je terecht?', 'color' => $this->color]);
     }
