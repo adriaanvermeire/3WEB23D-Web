5f1cf9ae8fc06991dece9f2d2a834fddb6498891
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-05 22:02:43 +0200

    implement alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,6 @@
-<div class="alert">
-    
-</div>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ $slot }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Sluiten">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
