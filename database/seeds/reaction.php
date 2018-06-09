df6ca13b603e99d13de0272a39bd1d1e7758c6d2
MaximGns <maxim.ganses@student.kdg.be>
2018-06-09 12:15:45 +0200

    seeder met content
    seeder reaction met content

---
+++ database/seeds/reaction.php
@@ -0,0 +1,22 @@
+<?php
+
+  $reactionblocks = [
+
+    [   'name'=>'We gingen met de klas zwemmen, in de kleedkamer vroeg een jongen aan mij of dat hij aan mijn piemel mocht komen, dan mocht ik ook aan zijn piemel komen. Ik vond dit niet leuk en wou dit ook niet doen.',
+        'good'=>'Zeg duidelijk ‘nee’ tegen de jongen op een vriendelijke manier. Maak duidelijk dat je dit niet wilt.',
+        'bad'=>'Je laat het niet zien dat je het niet wilt en laat het maar passeren alsof er niks is gebeurd.'],
+        
+        [   'name'=>'Ik trek zowel van jongens als van meisjes op de speelplaats hun broek naar beneden om eens goed te kunnen lachen.',
+        'good'=>'Denk na hoe het zou zijn als iemand anders het bij jou doet. De leeftijdsgenoten waarbij je dit doet kunnen zich schamen en ongemakkelijk in hun vel gaan voelen. ',
+        'bad'=>'je blijft het doen en schenkt geen aandacht aan andere hun gevoelens.'],
+
+        [   'name'=>'Je vriend op school roept tegen een meisje om hun borsten eens te laten zien.',
+        'good'=>'Je spreekt je vriend hierover aan en vertelt hem dat dit geen gepast gedrag is. Je vraagt hem om zich te verontschuldigen aan het meisje. Als dit vaker gebeurt adviseer je het meisje om de leerkracht hierbij te verwittigen.',
+        'bad'=>'Je gaat mee in zijn spelletje en daagt het meisje ook uit.'],
+
+        [   'name'=>'Je krijgt een briefje in de klas van een klasgenoot en eist dat je hem na school moet kussen, anders gaat je klasgenoot je geheimen doorvertellen aan anderen.',
+        'good'=>'reageer niet op het briefje, maar ga na de les naar je leerkracht.',
+        'bad'=>'laat je doen en je ziet hem na school, je gaat tegen je goesting in op zijn eisen. Aangezien jij je niet verzet hebt is het mogelijk dat je klasgenoot dit bij anderen en jezelf opnieuw kan proberen.'],
+  ];
+
+  
