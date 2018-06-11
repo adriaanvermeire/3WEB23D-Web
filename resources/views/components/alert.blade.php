9d7afa7915cdd3a12e023a5ff72e14ca9b532f01
adriaanvermeire <22325404+adriaanvermeire@users.noreply.github.com>
2018-06-11 18:56:24 +0200

    make alerts fixed at the top of the sreen

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-<div class="top-alert alert alert-success alert-dismissible slide" role="alert">
+<div class="fixed-top top-alert alert alert-success alert-dismissible slide" role="alert">
     <b>{{ $slot }}</b>
     <button type="button" class="close" data-dismiss="alert" aria-label="Sluiten">
       <span aria-hidden="true">&times;</span>
